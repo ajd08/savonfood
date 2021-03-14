78d4252a52739d57810639f9ce2757221bb1ad0a
anthonyd <anthonydang8@hotmail.com>
2021-03-14 11:32:06 -0400

    Add test for nearest no frills store

---
+++ api_gateway/src/helper/_scraper-getPostalCode.test.ts
@@ -0,0 +1,12 @@
+import {getNoFrillsLocation} from "./_scraper-getPostalCode";
+import {logger} from "../logger/config";
+
+test("Get nearest no frills location", async () => {
+    jest.setTimeout(30000);
+    const postal_code: string = "m1w2y5";
+    const store_postal_code = await getNoFrillsLocation(postal_code);
+    await expect(store_postal_code).toEqual("m1t3l4");
+    logger.info(JSON.stringify(store_postal_code));
+});
+
+

52c1f7858827016e8afc5097c910f8e6601e7737
anthonyd <anthonydang8@hotmail.com>
2021-03-15 22:04:52 -0400

    Add atoms to toggle LocationSearch State, recipes

--- frontend/src/atoms.tsx
+++ frontend/src/atoms.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React from "react";
 import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+} from "recoil";
+import { useHistory } from "react-router-dom";
+
+const axios = require("axios");
 
 const locationState = atom({
-    key: 'locationState',
-    default: '',
+    key: "locationState",
+    default: "",
 });
 
-export { locationState };
+const counterState = atom({
+    key: "counterState",
+    default: 0,
+});
 
+const locationSearchState = selector({
+    key: "locationSearchState",
 
+    get: async ({ get }) => {
+        const location = get(locationState);
+        const recipes = get(currentRecipesQuery);
+        if(location!="" && recipes!=[]) {
+            return false;
+        }
+    },
+});
+
+const currentRecipesQuery = selector({
+    key: "currentRecipes",
+    get: async ({ get }) => {
+        const location = get(locationState);
+        const response = await axios({
+            method: "get",
+            url: "http://localhost:8080/?postalcode=" + location,
+        });
+        return response.data;
+    },
+});
+
+export {
+    locationState,
+    locationSearchState,
+    currentRecipesQuery,
+    counterState,
+};

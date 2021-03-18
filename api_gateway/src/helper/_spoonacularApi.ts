2616a775800fe46ddfba768a17e02a8093487199
anthonyd <anthonydang8@hotmail.com>
2021-03-18 13:18:35 -0400

    Add api keys from spoonacular

--- api_gateway/src/helper/_spoonacularApi.ts
+++ api_gateway/src/helper/_spoonacularApi.ts
@@ -5,10 +5,14 @@ import { logger } from "../logger/config";
 const axios = require("axios");
 
 const apiKeys: string[] = [
-    "766e0045c601414e9ec04bd9fa363a9f",
-    "b75d71f41f7e437fa334d2aff5b5c568",
-    "2f690765657c46a985f0cd65028fc4fa",
-    "482150f6d831442382adfaf9a8e2085c",
+    //"766e0045c601414e9ec04bd9fa363a9f",
+    //"b75d71f41f7e437fa334d2aff5b5c568",
+    //"2f690765657c46a985f0cd65028fc4fa",
+    //"482150f6d831442382adfaf9a8e2085c",
+    //"a0cba7000d3042c3bbc1a59ab7b28add",
+    "925e595bb1fe4c0faad62aa1b8dc6d4f",
+    "3967327a427648f6a41f8fd5c7583da7",
+    "12b44df98e774449b7e4075a7cbaf8c5"
 ];
 
 interface Options {
@@ -16,25 +20,28 @@ interface Options {
     url: string;
     data: any;
 }
-const axiosRequest = async (options: Options, apiKeys: string[]) => {
+const axiosRequest = async (options: Options) => {
     const url_SELECTOR = options.url;
     for (let i = 0; i < apiKeys.length; i++) {
         let api_key: string = apiKeys[i];
         let url = url_SELECTOR.replace("$APIKEY", api_key);
-        options.url=url;
+        options.url = url;
         try {
+            logger.log("info", "URL USED: " + options.url);
             const response = await axios(options);
             const data = response.data;
             logger.info(
                 "(" +
-                    (i + 1).toString() +
+                    (i).toString() +
                     "/" +
                     apiKeys.length.toString() +
                     ") api keys used!"
             );
             return data;
         } catch (e) {
-            //logger.error("API Key out of juice: " + api_key, e);
+            logger.error("API Key out of juice: " + api_key, e);
+            //throw new Error("Error at axios request ->" + e);
+            continue;
         }
     }
     throw new Error("All API keys used");
@@ -63,12 +70,11 @@ const getProductInfo = async (item_title: string) => {
         },
     };
     try {
-        const data = await axiosRequest(options, apiKeys);
-        logger.info(JSON.stringify(data));
+        const data = await axiosRequest(options);
         return data;
     } catch (e) {
-        logger.error("getProductInfo not working", e);
-        return e;
+        logger.error("getProductInfo not working ->", e);
+        throw e;
     }
 };
 
@@ -109,14 +115,12 @@ const getRecipes = async (numRecipes: number, ingredients: string[]) => {
     };
 
     try {
-        const data = await axiosRequest(options, apiKeys);
-        logger.info(JSON.stringify(data));
+        const data = await axiosRequest(options);
+        logger.info("Got all RECIPES!!");
         return data;
     } catch (e) {
-        logger.error("error for getRecipes", e)
-        return e;
-    } finally {
-        logger.log("info", "getProductInfo: DONE!");
+        logger.error("error for getRecipes", e);
+        throw e;
     }
 };
 
@@ -136,21 +140,20 @@ const getRecipe = async (recipeID: number) => {
         recipeID.toString() +
         "/information?apiKey=$APIKEY";
 
-    const options : Options = {
+    const options: Options = {
         method: "get",
         url: url,
         data: {},
     };
 
     try {
-        const data = await axiosRequest(options,apiKeys);
-        logger.info(JSON.stringify(data,null,2));
+        const data = await axiosRequest(options);
+        logger.info("Got recipe succeess!! ->" + url);
         return data;
     } catch (e) {
-        return e;
-    } finally {
-        logger.log("info", "getRecipe: DONE!");
+        logger.error("error for getRecipes", e);
+        throw e;
     }
 };
 
-export { getRecipes, getRecipe, getProductInfo, apiKeys};
+export { getRecipes, getRecipe, getProductInfo, apiKeys };

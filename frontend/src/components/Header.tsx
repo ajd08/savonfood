f5cc0f4d732a94fd0256b361171941805e3f8941
anthonyd <anthonydang8@hotmail.com>
2021-03-20 01:08:48 -0400

    Change header link and name of app

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
         <Router>
             <div className="Header-container">
                 <header id="Header">
-                    <h2>BASiL</h2>
+                    <h2>THYME</h2>
                 </header>
                 <div className="header-navigation">
                     <Link className="link-appetizers" to="/home">APPETIZERS</Link>
                     <Link className="link-entrees" to="/home">ENTRÉES</Link>
                     <Link className="link-desserts" to="/home">DESSERTS</Link>
-                    <Link className="link-cocktails" to="/home">COCKTAILS</Link>
+                    <Link className="link-cocktails" to="/home">About</Link>
                 </div>
             </div>
         </Router>

273bc4c329a74226f46a8b5f03254d7d66c4ab50
anthonyd <anthonydang8@hotmail.com>
2021-03-26 22:28:32 -0400

    Add link to the logo to go to homepage

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import { useHistory } from "react-router-dom";
+
 const Header = () => {
+    const history = useHistory();
+
+
+    const onClick = () => (event: any) => {
+        history.push('/');
+    };
+
     return (
         <Router>
             <div className="Header-container">
-                <header id="Header">
-                    <h2>THYME</h2>
-                </header>
+                <Link to="/" onClick={onClick()}>
+                    <header id="Header">
+                        <h2>THYME</h2>
+                    </header>
+                </Link>
                 <div className="header-navigation">
-                    <Link className="link-appetizers" to="/home">APPETIZERS</Link>
-                    <Link className="link-entrees" to="/home">ENTRÉES</Link>
-                    <Link className="link-desserts" to="/home">DESSERTS</Link>
-                    <Link className="link-cocktails" to="/home">About</Link>
+                    <Link className="link-appetizers" to="/home">
+                        APPETIZERS
+                    </Link>
+                    <Link className="link-entrees" to="/home">
+                        ENTRÉES
+                    </Link>
+                    <Link className="link-desserts" to="/home">
+                        DESSERTS
+                    </Link>
+                    <Link className="link-cocktails" to="/home">
+                        About
+                    </Link>
                 </div>
             </div>
         </Router>

2f52243588f49832a194e7abaf38d4753caa6827
anthonyd <anthonydang8@hotmail.com>
2021-04-02 02:45:03 -0400

    Get spoonacular api from the db

---
+++ api_gateway/src/helper/private_postgraphile.ts
@@ -0,0 +1,58 @@
+const axios = require("axios");
+//const host_url: string = process.env["GRAPHQL_URL"]
+//URL ONLY USED FOR DEVELOPMENT
+const host_url: string = "http://0.0.0.0:5433/graphql";
+
+const getSpoonacularKey = async() => {
+    const options = {
+        url: host_url,
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+        },
+        data: {
+            query: `
+                query getKey {
+                  spoonacularKeys(first: 1, condition: {used: false}) {
+                    edges {
+                      node {
+                         id
+                         key
+                         nodeId
+                         used
+                      }
+                    }
+                  }
+                }
+            `,
+        },
+    };
+    const response = await axios(options);
+    const data = response.data;
+    return data;
+};
+
+const updateUsedSpoontacularKey = async(key_id : number) => {
+    const options = {
+        url: host_url,
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+        },
+        data: {
+            query: `
+                mutation updateUsedKey {
+                  updateSpoonacularKey(input: {patch: {used: true}, id: ${key_id.toString()}}) {
+                    clientMutationId
+                  }
+                }
+            `,
+        },
+    };
+    const response = await axios(options);
+    const data = response.data;
+    return data;
+};
+export {getSpoonacularKey, updateUsedSpoontacularKey};
